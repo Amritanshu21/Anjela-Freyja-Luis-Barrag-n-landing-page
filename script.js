955fbaa84435cb6d896b602d22243dce328a0bd3
Amritanshu21 <120729168+Amritanshu21@users.noreply.github.com>
2023-10-24 01:39:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+ gsap.from("#image1",{
+    y:150,
+    duration:1,
+    opacity:0,
+    scale:1.5
+ })
+ gsap.from("#image2",{
+    y:150,
+    duration:1,
+    opacity:0,
+    scale:1.5
+ })
+ gsap.from("#image3",{
+    y:150,
+    duration:1,
+    opacity:0,
+    scale:1.5
+ })
+ gsap.from("#image4",{
+    y:150,
+    duration:1,
+    opacity:0,
+    scale:1.5
+ })
+ 
+ 
